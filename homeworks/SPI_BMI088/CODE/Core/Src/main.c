e66fa28defb00554b15737c2ad2128fe058a9f12
Faneter <Faneter@outlook.com>
2025-12-16 17:17:37 +0800

    BMI088外设代码

--- homeworks/SPI_BMI088/CODE/Core/Src/main.c
+++ homeworks/SPI_BMI088/CODE/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "bmi088.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -89,7 +89,7 @@ int main(void)
   MX_GPIO_Init();
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-
+  BMI088_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
